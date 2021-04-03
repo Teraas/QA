0f04ec35ac7fde014689e877bcc9be22e77439c3
harish.kumar <harish.kumar@servicenow.com>
2021-04-03 23:22:06 +0530

    more tests

--- Frameworks/API Automation Framework Java/src/test/java/Tests/SampleITTest.java
+++ Frameworks/API Automation Framework Java/src/test/java/Tests/SampleITTest.java
@@ -1,15 +1,22 @@
 package Tests;
 
 import static io.restassured.RestAssured.*;
+import static java.util.Arrays.asList;
+
+import java.util.Collection;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.*;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import Utilities.Util;
 import DomainObjects.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class SampleITTest {
 	private static RequestSpecification requestSpec;
 	@BeforeClass
@@ -21,7 +28,7 @@ public class SampleITTest {
 				build();
 	}
 	@Test
-	public void verifyDummyObjectCall() {
+	public void verifyDummyObjectCallWithDefautName() {
 		System.out.println(DummyModel.builder().withDefaultAttributes().build());
 		given().
 				spec(requestSpec).
@@ -32,4 +39,28 @@ public class SampleITTest {
 				then().
 				assertThat().equals("A response object");
 	}
+
+	@Test
+	@Parameters(method = "parametersForValidateValidMessages")
+	public void verifyDummyObjectCallWithName(String name, String response) {
+		System.out.println(DummyModel.builder().withName(name).build());
+		given().
+				spec(requestSpec).
+				and().
+				body( DummyModel.builder().withDefaultAttributes().toString() ).
+				when().
+				post("/api/v1/test/").
+				then().
+				assertThat().equals(response);
+	}
+
+	public static Collection<Object[]> parametersForValidateValidMessages() {
+		return asList(
+				new Object[][]{
+						{ "Test1", "response"},
+						{ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "response" },
+						{ "", "response"},
+				});
+	}
+
 	}
